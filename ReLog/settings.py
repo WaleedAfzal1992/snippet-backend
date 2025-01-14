3e72457cdef7dc9d2d0287aa1f86452400a0bb59
WaleedAfzal1992 <143810605+WaleedAfzal1992@users.noreply.github.com>
2025-01-14 17:54:19 +0500

    Update settings.py

--- ReLog/settings.py
+++ ReLog/settings.py
@@ -1,6 +1,7 @@
 
 from datetime import timedelta
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,6 +151,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
